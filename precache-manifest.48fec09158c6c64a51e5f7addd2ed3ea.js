1fb463fe64252cc7b1779f54d9fcff6b461261fd
Nishant <nishants440@gmail.com>
2020-11-01 07:36:41 +0530

    Updates

---
+++ precache-manifest.48fec09158c6c64a51e5f7addd2ed3ea.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508372041527680da4bf875fdc2edf3d",
+    "url": "/MerryWeather/index.html"
+  },
+  {
+    "revision": "991ae42bd7c9d1ee87ef",
+    "url": "/MerryWeather/static/css/2.206980a0.chunk.css"
+  },
+  {
+    "revision": "c87150f64a9685a23514",
+    "url": "/MerryWeather/static/css/main.c2a1d837.chunk.css"
+  },
+  {
+    "revision": "991ae42bd7c9d1ee87ef",
+    "url": "/MerryWeather/static/js/2.8c89bae1.chunk.js"
+  },
+  {
+    "revision": "e6ff33bd71c9fe5f55528ac2d613cee5",
+    "url": "/MerryWeather/static/js/2.8c89bae1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c87150f64a9685a23514",
+    "url": "/MerryWeather/static/js/main.f4dced63.chunk.js"
+  },
+  {
+    "revision": "e96d8a0970c8a75a512f",
+    "url": "/MerryWeather/static/js/runtime-main.2373fa50.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/MerryWeather/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/MerryWeather/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/MerryWeather/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/MerryWeather/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/MerryWeather/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/MerryWeather/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/MerryWeather/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/MerryWeather/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/MerryWeather/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/MerryWeather/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/MerryWeather/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "18bcf5b5d511e4c6ebf4ab4de4b0aa1a",
+    "url": "/MerryWeather/static/media/jeremy-bishop-8xznAGy4HcY-unsplash.18bcf5b5.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/MerryWeather/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/MerryWeather/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/MerryWeather/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/MerryWeather/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/MerryWeather/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
